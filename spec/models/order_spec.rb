377c1bc97a6f4db86cd5854e9a4b8dbeb3904a73
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-07-09 21:12:24 -0600

    updated order spec to define required behavior

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Order do
   describe 'relationships' do
     it { should have_many :line_items }
   end
+
+  describe 'instance methods' do
+    let(:order) { Order.create }
+    let(:line_item1) { LineItem.new(description: 'imported item', price: 47.50, quantity: 1) }
+    let(:line_item2) { LineItem.new(description: 'imported chocolate', price: 10.00, quantity: 1) }
+    before do
+      order.line_items << line_item1
+      order.line_items << line_item2
+      line_item1.save
+      line_item2.save
+    end
+
+    it 'calculate_total' do
+      expect(order.calculate_order_total).to eq(65.15)
+    end
+
+    it 'calculate_sales_taxes' do
+      expect(order.calculate_sales_tax).to eq(7.65)
+    end
+  end
 end
