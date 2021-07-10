a593c0227cf28bd8b7fb6f8601400271750a1053
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-07-09 20:48:22 -0600

    Update LineItem model spec for required behavior

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -14,4 +14,40 @@ RSpec.describe LineItem do
     it { should belong_to :order }
     it { should have_many(:tax_categories).through(:tax_associations) }
   end
+
+  describe 'Instance Methods' do
+    describe 'tax_categories' do
+      context 'with single tax category' do
+        subject do
+          described_class.create(description: 'chocolate bar', price: 0.85, quantity: 1, order_id: Order.create.id)
+        end
+
+        it { expect(subject.tax_categories.first.name).to eq('Food') }
+      end
+
+      context 'with multiple tax categories' do
+        subject do
+          described_class.create(description: 'imported chocolates',
+                                 price: 10.00,
+                                 quantity: 1,
+                                 order_id: Order.create.id)
+        end
+
+        it { expect(subject.tax_categories.first.name).to eq('Food') }
+        it { expect(subject.tax_categories.last.name).to eq('Imported') }
+      end
+
+      context 'with Not a food, medicine or book' do
+        subject do
+          described_class.create(description: 'imported perfume bottle',
+                                 price: 47.50,
+                                 quantity: 1,
+                                 order_id: Order.create.id)
+        end
+
+        it { expect(subject.tax_categories.first.name).to eq('Standard Item') }
+        it { expect(subject.tax_categories.last.name).to eq('Imported') }
+      end
+    end
+  end
 end
