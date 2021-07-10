444c8a1a42a2f6fe48a2dcbd652055bea34cc51e
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-07-09 21:13:49 -0600

    updated order methods
    This commit adds three new methods:
    calculate_order_total - sums line item totals
    calculate_sales_taxes - sums line item taxes
    update_totals - this method will update the order after created/updated

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,20 @@
 
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def update_totals
+    update(order_total: calculate_order_total, sales_tax: calculate_sales_tax)
+  end
+
+  def calculate_sales_tax
+    return if line_items.empty?
+
+    line_items.sum(:total_taxes)
+  end
+
+  def calculate_order_total
+    return if line_items.empty?
+
+    line_items.sum(:adjusted_price)
+  end
 end
