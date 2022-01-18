32b9f30d59816d49e9e61c14803485e84522d133
Safa ABALLAGH <aballaghsafa97@gmail.com>
2022-01-18 10:22:40 +0100

    Update taxes and total price for basket

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,7 +1,32 @@
 class Basket < ApplicationRecord
   has_many :goods, dependent: :destroy
 
+  before_save :update_taxes_update_price
+
   def handle_file(file)
     file.read.split("\n")
+  end
+
+  private
+
+  def update_taxes_update_price
+    self.sales_taxes = sum_tax
+    self.total = sum_total
+  end
+
+  def sum_tax
+    sum = 0
+    goods.each do |entry|
+      sum += entry.basic_tax.to_f + entry.import_tax.to_f
+    end
+    sum.round(2)
+  end
+
+  def sum_total
+    sum = 0
+    goods.each do |entry|
+      sum += entry.final_price.to_f
+    end
+    sum.round(2)
   end
 end
