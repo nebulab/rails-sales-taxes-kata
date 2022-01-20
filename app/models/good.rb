0983ba9b7ccde936854fba2c8bf79747d90b1352
Safa ABALLAGH <aballaghsafa97@gmail.com>
2022-01-20 17:58:40 +0100

    Refactor good model & add magic numbers

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,4 +1,9 @@
 class Good < ApplicationRecord
+  EXEMPT_FROM_TAX = %w[chocolate book pills].freeze
+  BASIC_TAX_RATE = 0.1
+  IMPORT_TAX_RATE = 0.05
+  PRESITION = 20.0
+
   belongs_to :basket
 
   before_save :calulate_tax_update_price
@@ -6,16 +11,26 @@ class Good < ApplicationRecord
   private
 
   def calulate_tax_update_price
-    exampt = %w[chocolate book pills]
-    self.basic_tax = round_up(price.to_f.round(2) * 0.1) unless exampt.any? do |exeption|
-                                                                  name.include?(exeption)
-                                                                end
-    self.import_tax = round_up(price.to_f.round(2) * 0.05) if name.include?('imported')
-    fprice = quantity.to_f * (price.to_f.round(2) + basic_tax.to_f + import_tax.to_f)
-    self.final_price = fprice.round(2)
+    self.basic_tax = calcul_basic_tax(price, name)
+    self.import_tax = calcul_import_tax(price, name)
+    self.final_price = calcul_final_price(quantity, price, basic_tax, import_tax)
+  end
+
+  def calcul_basic_tax(price, name)
+    round_up(price.to_f.round(2) * BASIC_TAX_RATE) unless EXEMPT_FROM_TAX.any? do |exeption|
+      name.include?(exeption)
+    end
+  end
+
+  def calcul_import_tax(price, name)
+    round_up(price.to_f.round(2) * IMPORT_TAX_RATE) if name.include?('imported')
+  end
+
+  def calcul_final_price(quantity, price, basic_tax, import_tax)
+    (quantity.to_f * (price.to_f.round(2) + basic_tax.to_f + import_tax.to_f)).round(2)
   end
 
   def round_up(value)
-    (value * 20).ceil / 20.0
+    (value * PRESITION).ceil / PRESITION
   end
 end
