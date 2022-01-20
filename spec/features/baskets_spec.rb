ab1c73d2c4f71b938bf6a6d2451675ccf9d62e54
Safa ABALLAGH <aballaghsafa97@gmail.com>
2022-01-20 17:28:05 +0100

    Refactor baskets_spec.rb add before method

--- spec/features/baskets_spec.rb
+++ spec/features/baskets_spec.rb
@@ -1,31 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Basket upload and show receipt', type: :feature do
-  it 'uploads basket' do
+  before :each do
     visit '/baskets/new'
-    page.attach_file('file', './basket_2.txt')
+    page.attach_file('file', 'baskets/basket_2.txt')
     click_button 'submit'
+  end
+
+  it 'uploads basket' do
     expect(page).to have_content 'imported bottle of perfume'
   end
 
   it 'calculate Sales taxes' do
-    visit '/baskets/new'
-    page.attach_file('file', './basket_2.txt')
-    click_button 'submit'
     expect(page).to have_content 'Sales taxes : 7.65'
   end
 
   it 'calculate Total' do
-    visit '/baskets/new'
-    page.attach_file('file', './basket_2.txt')
-    click_button 'submit'
     expect(page).to have_content 'Total: 65.15'
   end
 
   it 'Update goods price' do
-    visit '/baskets/new'
-    page.attach_file('file', './basket_2.txt')
-    click_button 'submit'
     expect(page).to have_content '1 imported bottle of perfume : 54.65'
   end
 end
